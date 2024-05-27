1b5a15b1c4d6776510b17ec94a3fdb9ef7ed093e
Yuriy Chekan <chekanyr@icloud.com>
2024-05-27 11:32:35 +0300

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Юрий Чекан on 27.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
